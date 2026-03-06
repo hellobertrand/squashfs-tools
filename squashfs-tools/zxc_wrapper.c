2c5c78a16e635f6ece2f4f065ba57a6a2a566f10
hellobertrand <5901952+hellobertrand@users.noreply.github.com>
2026-03-06 22:02:39 +0100

    zxc: Refactor to use thread-local reusable contexts
    Update the zxc compression and decompression wrappers to leverage the zxc library's Reusable Context API. Compression and decompression contexts are now created and managed using thread-local storage.
    This avoids repeated allocation and deallocation of contexts, improving performance, especially in multi-threaded environments.
    
    Adds copyright headers to the zxc_wrapper source and header files. 
    Updates default compression level constant to `ZXC_LEVEL_DEFAULT` for consistency with the zxc library.

--- squashfs-tools/zxc_wrapper.c
+++ squashfs-tools/zxc_wrapper.c
@@ -1,14 +1,42 @@
+/*
+ * Copyright (c) 2025-2026
+ * Bertrand Lebonnois
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * zxc_wrapper.c
+ *
+ * Support for ZXC compression using the Reusable Context API.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <zxc.h>
+#include <pthread.h>
 
 #include "squashfs_fs.h"
 #include "zxc_wrapper.h"
 #include "compressor.h"
 #include "print_pager.h"
 
-static int compression_level = ZXC_DEFAULT_COMPRESSION_LEVEL;
+static int compression_level = ZXC_LEVEL_DEFAULT;
+
+/* ========================================================================= */
+/*  Compressor option callbacks                                              */
+/* ========================================================================= */
 
 static int zxc_options(char *argv[], int argc)
 {
@@ -39,7 +67,7 @@ static void *zxc_dump_options(int block_size, int *size)
 {
 	static struct zxc_comp_opts comp_opts;
 
-	if (compression_level == ZXC_DEFAULT_COMPRESSION_LEVEL)
+	if (compression_level == ZXC_LEVEL_DEFAULT)
 		return NULL;
 
 	comp_opts.compression_level = compression_level;
@@ -54,7 +82,7 @@ static int zxc_extract_options(int block_size, void *buffer, int size)
 	struct zxc_comp_opts *comp_opts = buffer;
 
 	if (size == 0) {
-		compression_level = ZXC_DEFAULT_COMPRESSION_LEVEL;
+		compression_level = ZXC_LEVEL_DEFAULT;
 		return 0;
 	}
 
@@ -108,32 +136,117 @@ failed:
 
 static int zxc_squashfs_init(void **strm, int block_size, int datablock)
 {
-	/* ZXC buffer API does not require persistent context across blocks */
 	*strm = NULL;
 	return 0;
 }
 
+/* ========================================================================= */
+/*  Thread-local reusable contexts                                           */
+/* ========================================================================= */
+
+static pthread_key_t zxc_tls_comp_key;
+static pthread_key_t zxc_tls_decomp_key;
+static pthread_once_t zxc_tls_once = PTHREAD_ONCE_INIT;
+
+static void zxc_comp_tls_destructor(void *data)
+{
+	if (data)
+		zxc_free_cctx(data);
+}
+
+static void zxc_decomp_tls_destructor(void *data)
+{
+	if (data)
+		zxc_free_dctx(data);
+}
+
+static void zxc_tls_init(void)
+{
+	pthread_key_create(&zxc_tls_comp_key, zxc_comp_tls_destructor);
+	pthread_key_create(&zxc_tls_decomp_key, zxc_decomp_tls_destructor);
+}
+
+/* ========================================================================= */
+/*  Compression                                                              */
+/* ========================================================================= */
+
 static int zxc_squashfs_compress(void *strm, void *dest, void *src, int size,
 			 int block_size, int *error)
 {
-	int64_t res = zxc_compress(src, size, dest, block_size,
-				   compression_level, 1);
+	zxc_cctx *ctx;
+	zxc_compress_opts_t opts;
+	size_t bound;
+	uint8_t *bounce;
+	int64_t res;
 
-	if (res < 0) {
-		/* Return 0 to indicate failure to compress (e.g. incompressible) */
+	pthread_once(&zxc_tls_once, zxc_tls_init);
+
+	memset(&opts, 0, sizeof(opts));
+	opts.level = compression_level;
+	opts.block_size = block_size;
+	opts.checksum_enabled = 0;
+
+	ctx = pthread_getspecific(zxc_tls_comp_key);
+	if (!ctx) {
+		ctx = zxc_create_cctx(&opts);
+		if (!ctx)
+			return 0;
+		pthread_setspecific(zxc_tls_comp_key, ctx);
+	}
+
+	/*
+	 * zxc_compress_cctx produces a full ZXC archive (header + blocks +
+	 * footer).  We need a bounce buffer since the output may temporarily
+	 * exceed block_size even if the final payload is smaller.
+	 */
+	bound = zxc_compress_bound(size);
+	bounce = malloc(bound);
+	if (!bounce)
+		return 0;
+
+	res = zxc_compress_cctx(ctx, src, size, bounce, bound, &opts);
+
+	if (res <= 0 || res > block_size) {
+		free(bounce);
 		return 0;
 	}
+
+	memcpy(dest, bounce, res);
+	free(bounce);
 
 	return (int)res;
 }
 
+/* ========================================================================= */
+/*  Decompression                                                            */
+/* ========================================================================= */
+
 static int zxc_squashfs_uncompress(void *dest, void *src, int size, int outsize,
 			   int *error)
 {
-	int64_t res = zxc_decompress(src, size, dest, outsize, 1);
+	zxc_dctx *ctx;
+	zxc_decompress_opts_t opts;
+	int64_t res;
+
+	pthread_once(&zxc_tls_once, zxc_tls_init);
+
+	ctx = pthread_getspecific(zxc_tls_decomp_key);
+	if (!ctx) {
+		ctx = zxc_create_dctx();
+		if (!ctx) {
+			*error = -1;
+			return -1;
+		}
+		pthread_setspecific(zxc_tls_decomp_key, ctx);
+	}
+
+	memset(&opts, 0, sizeof(opts));
+	opts.checksum_enabled = 0;
+	res = zxc_decompress_dctx(ctx, src, size, dest, outsize, &opts);
 
 	if (res < 0) {
-		fprintf(stderr, "\t%d %d zxc error %d\n", outsize, size, (int)res);
+		fprintf(stderr, "\t%d %d zxc error %lld\n", outsize, size,
+			(long long)res);
 		*error = (int)res;
 		return -1;
 	}
@@ -141,10 +254,14 @@ static int zxc_squashfs_uncompress(void *dest, void *src, int size, int outsize,
 	return (int)res;
 }
 
+/* ========================================================================= */
+/*  Usage / help                                                             */
+/* ========================================================================= */
+
 static void zxc_usage(FILE *stream, int cols)
 {
 	autowrap_print(stream, "\t  -Xcompression-level <compression-level>\n", cols);
-	autowrap_printf(stream, cols, "\t\t<compression-level> should be 1 .. 5 (default %d).\n", ZXC_DEFAULT_COMPRESSION_LEVEL);
+	autowrap_printf(stream, cols, "\t\t<compression-level> should be 1 .. 5 (default %d).\n", ZXC_LEVEL_DEFAULT);
 }
 
 static int option_args(char *option)
--- squashfs-tools/zxc_wrapper.h
+++ squashfs-tools/zxc_wrapper.h
@@ -1,6 +1,28 @@
 #ifndef ZXC_WRAPPER_H
 #define ZXC_WRAPPER_H
-
+/*
+ * Squashfs
+ *
+ * Copyright (c) 2025-2026
+ * Bertrand Lebonnois
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * zxc_wrapper.h
+ *
+ */
 #include "endian_compat.h"
 
 #if __BYTE_ORDER == __BIG_ENDIAN
@@ -13,9 +35,6 @@ extern unsigned int inswap_le32(unsigned int);
 #else
 #define SQUASHFS_INSWAP_COMP_OPTS(s)
 #endif
-
-/* Default compression level */
-#define ZXC_DEFAULT_COMPRESSION_LEVEL 3
 
 struct zxc_comp_opts {
 	int compression_level;
